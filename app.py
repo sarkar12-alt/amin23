68169bbe337c215e927ae10d06a9356e4a7f6c05
sarkar12-alt <aminsarkar9635@gmail.com>
2026-04-05 09:12:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Amin 🚀"
+    return "Hello Amin 🚀 Your app is working!"
+
+if __name__ == "__main__":
+    app.run()
